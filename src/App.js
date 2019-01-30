acad0dc060a5b0eed36df04f2b28ae02815eeafd
corine <filterlee@gmail.com>
2019-01-30 13:24:14 +0900

    connect client to server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: "고 홈 좌니!!"
+    };
+  }
+  async componentDidMount() {
+    await axios
+      .post("/users/login", {
+        userid: "Super",
+        password: "Sexy"
+      })
+      .then(
+        this.setState({
+          login: "이것이 로그인이다"
+        })
+      );
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{this.state.login}</h1>
       </div>
     );
   }
